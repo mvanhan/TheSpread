e40bfc191d75c0a19c997f0514af968deff7d62e
Miller Van Hanken <mvanhan@unc.edu>
2024-06-11 15:00:33 -0500

    new spacing

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './News.css';
 
-
 const News = () => {
   return (
       <div className="newsPage">
-          <div className="rightContainer">
+          <div className="leftContainer">
               <div className="locksBox">
-                  <h2>Lock Streak:       0</h2> 
+                  <h2>Lock Streak: 0</h2> 
               </div>
           </div>
-          {/* <div className="middleContainer">
+          <div className="middleContainer">
               <div className="locksBox">
-                  <h2>Lock Streak:       0</h2> 
+                  <h2>Lock Streak: 1</h2> 
               </div>
-          </div> */}
+          </div>
+          <div className="rightContainer">
+              <div className="locksBox">
+                  <h2>Lock Streak: 2</h2> 
+              </div>
+          </div>
       </div>
   );
 }
