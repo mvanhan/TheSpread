02d700281bf34245d8cfe207c152062a890c28ff
Miller Van Hanken <mvanhan@unc.edu>
2024-07-16 09:35:48 -0500

    First Posts

--- src/pages/News.js
+++ src/pages/News.js
@@ -9,33 +9,50 @@ const News = () => {
   return (
       <div className="newsPage">
           <div className="leftContainer">
-              <div className="locksBox">
+              <div className="pollBox">
                   <h2>Who do you fucking hate more?</h2> 
                   <Poll />
               </div>
               <br></br>
-              <div className="locksBox">
+              <div className="pollBox">
                   <h2>Who's uglier?</h2> 
                   <Poll2 />
               </div>
               <br></br>
-              <div className="locksBox">
-              </div>
           </div>
           <div className="middleContainer">
+          <div className="midBox">
+                  <h2>How Bronny’s poor performance has RUINED Lebron's Legacy</h2>
+                  <br></br>
+                  <p>Lebron had a set plan to become the official GOAT of basketball. Lesperm has not just become a nagging problem to Lebron’s legacy through this nepotism scandal, but also a problem with the birds. Our sources say Bronny off the court gets no play and only gets with dusters putting him in a shooting slump on the court yet still hammering 3s off. 
+                  </p>
+              </div>
+              <br></br>
               <div className="midBox">
-                  <h2>I hate Jason Tatum's corny ass so damn much he's worse than fucking Klay.</h2> 
+                  <h2>“Bronny will never be a pro” - Jaylen Brown to his shawty Angel Reese
+                  </h2>
+                  <br></br>
+                  <p>So basically Jaylen is super butthurt about not making team USA and is trying to slide in to ugly ass Angel Reese while his girl is right next to him. Not sure how this will work out for JB but at least he got carried by Brad Stevens for a chip. 
+                  </p>
+              </div>
+              <br></br>
+              <div className="midBox">
+                  <h2>Why Rory Mcllroy will finally win another Major
+                  .</h2>
+                  <br></br>
+                  <p>Rory’s wife has called for a divorce one month before The Open. Not only has Rory been more dialed than usual in recent tournaments, but this bitch actually had the nerve to show up to the tournament. Rory isn’t fucking around and is about to get a W which means a massive W for the golf world. #FuckLIV
+                  </p>
               </div>
           </div>
           <div className="rightContainer">
               <div className="locksBox">
-                  <h2>Lock Streak: 1</h2> 
+                  <h2>Lock Streak: 3</h2> 
               </div>
               <br></br>
               <div className="locksBox">
                   <h2>LOTD 7/12</h2>
                   <br></br>
-                  <p>P000000P</p>
+                  <p>NL Moneyline</p>
               </div>
               <br></br>
               <div className='midBox'>
