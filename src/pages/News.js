3131eed9cde5c5d722f7b2311209a3fd84a4785d
Miller Van Hanken <mvanhan@unc.edu>
2024-08-01 12:10:18 -0500

    daily

--- src/pages/News.js
+++ src/pages/News.js
@@ -22,6 +22,20 @@ const News = () => {
           </div>
           <div className="middleContainer">
             <div className="midBox">
+                  <h2>USA Lacking in Gold?</h2>
+                  <br></br>
+                  <p>Heading into Thursday at the Olympics, the United States currently only has 6 gold medals, 5 behind the leader of China. The US does lead the total medal count but gold is all that matters tbh. 
+                  </p>
+              </div>
+              <br></br>
+            <div className="midBox">
+                  <h2>Team USA Revenge</h2>
+                  <br></br>
+                  <p>LeAmerica and company took care of business Monday against South Sudan after the previous close call. Steph Curry shot 1-9 because of a lack of sex and Embiid didn’t play because he was watching Naruto on the bench. 
+                  </p>
+              </div>
+              <br></br>
+            <div className="midBox">
                   <h2>Expectations of MLB Trade Deadline</h2>
                   <br></br>
                   <p>Honestly, no big deals are going to happen and even if they did you have no fucking clue who they are. 26 days till college football ladies and gentleman. 
@@ -46,42 +60,6 @@ const News = () => {
                   <h2>Lincoln Riley WILL be Fired by 2025 Season</h2>
                   <br></br>
                   <p>Riley at the Big 10 media day admitted “Yea I didn’t want to go to the SEC i’d be scared shitless” and also admitted they need to take Notre Dame (historic rivalry) off their schedule for them to ever have a chance at a Natty. We all know this won’t happen anyways because Lincoln can’t put a defense on the field and doesn’t have Caleb to carry him anymore. 
-                  </p>
-              </div>
-              <br></br>
-              <div className="midBox">
-                  <h2>The Secret Behind the Heat’s Summer League Title: A Snow Bunny</h2>
-                  <br></br>
-                  <p>Josh Christopher dropped 24 points, 6-10 from 3, in Monday’s Summer League final winning himself the MVP. This is all thanks to Jalen Green who apparently stole his snow bunny back in Houston. Christopher is searching for his revenge as he is reviving his NBA career in looks of a new snow bunny. 
-                  </p>
-              </div>
-              <br></br>
-              <div className="midBox">
-                  <h2>WWE Star Logan Paul sued by the Olympics</h2>
-                  <br></br>
-                  <p>Honestly, this dude is just a clown and his drink tastes like if a dog took a shit in it. Logan couldn’t even win Mr. Beast’s challenge because he’s so retarded and then just thought it was chill to use Olympics on his product. Fuck you Logan. 
-                  </p>
-              </div>
-              <br></br>
-              <div className="midBox">
-                  <h2>How Bronny’s poor performance has RUINED Lebron's Legacy</h2>
-                  <br></br>
-                  <p>Lebron had a set plan to become the official GOAT of basketball. Lesperm has not just become a nagging problem to Lebron’s legacy through this nepotism scandal, but also a problem with the birds. Our sources say Bronny off the court gets no play and only gets with dusters putting him in a shooting slump on the court yet still hammering 3s off. 
-                  </p>
-              </div>
-              <br></br>
-              <div className="midBox">
-                  <h2>“Bronny will never be a pro” - Jaylen Brown to his shawty Angel Reese
-                  </h2>
-                  <br></br>
-                  <p>So basically Jaylen is super butthurt about not making team USA and is trying to slide in to ugly ass Angel Reese while his girl is right next to him. Not sure how this will work out for JB but at least he got carried by Brad Stevens for a chip. 
-                  </p>
-              </div>
-              <br></br>
-              <div className="midBox">
-                  <h2>Why Rory Mcllroy will finally win another Major</h2>
-                  <br></br>
-                  <p>Rory’s wife has called for a divorce one month before The Open. Not only has Rory been more dialed than usual in recent tournaments, but this bitch actually had the nerve to show up to the tournament. Rory isn’t fucking around and is about to get a W which means a massive W for the golf world. #FuckLIV
                   </p>
               </div>
           </div>
